b48f8bc5c0209f2a5a07f8f39ac459b314d3a26d
dibkb <dibas9110@gmail.com>
2024-02-04 21:48:40 +0530

    closure

--- 1/script.js
+++ 1/script.js
@@ -6,3 +6,15 @@ function hello() {
   var first = "third";
   console.log(first);
 }
+// closure;
+function outer() {
+  const x = 7;
+  return function inner() {
+    console.log(x);
+  };
+}
+const f = outer();
+// console.log(f);
+const x = 9;
+console.log(x);
+f();
